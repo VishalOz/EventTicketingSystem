238acd21ceed0eeaaf99236af1da0ba295de8e54
Vishal Oshada <vishaloshada@Vishals-MacBook-Air.local>
2025-05-28 13:46:50 +0530

    did some stuff

--- User.java
+++ User.java
@@ -4,11 +4,26 @@ import java.util.List;
 public class User {
     private String email;
     private String password;
+    private int ticketCount;
     private List<String> bookings;
 
-    public User(String email, String password) {
+    public User(String email, String password, int ticketCount) {
         this.email = email;
         this.password = password;
+        this.ticketCount = ticketCount;
         this.bookings = new ArrayList<>();
+    }
+
+    public void bookEvent(Event event) {
+        bookings.add(event);
+    }
+
+    public void cancelBooking(int index) {
+        if (index >= 0 && index < bookings.size()) {
+            bookings.remove(index);
+        }
+    }
+    public List<Event> getBookings() {
+        return bookings;
     }
 }
